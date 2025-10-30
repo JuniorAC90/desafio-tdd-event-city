a64f2f86151c4345b4dee57e4ae449b89e5c6844
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-10-29 23:16:09 -0300

    Created findAll

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = repository.findAll(Sort.by("name"));
+        return list.stream().map(c -> new CityDTO(c)).collect(Collectors.toList());
+    }
+}
