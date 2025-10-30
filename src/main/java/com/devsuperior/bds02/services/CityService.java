d00922dc330a67e92cc55fc25bb32c2c38cc2f22
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-10-29 23:31:02 -0300

    Created insert

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -23,4 +23,12 @@ public class CityService {
         List<City> list = repository.findAll(Sort.by("name"));
         return list.stream().map(c -> new CityDTO(c)).collect(Collectors.toList());
     }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
 }
